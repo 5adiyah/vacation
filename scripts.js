73e01a0b3db9c8e6a7c35f1b55930a45d44aa994
5adiyah <sadiali@pdx.edu>
2016-02-24 20:19:55 -0800

    Did some jquery, doesn't work though

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+  $('#formwrapper form').submit(function(event){
+    var weather = parseInt($("#sweather").val());
+    var vacationreason = parseInt($("#svacationreason").val());
+    var aboutyou = parseInt($("#saboutyou").val());
+    var activity = parseInt($("#sactivity").val());
+    var home = parseInt($("#shome").val());
 
+    // var points = weather + vacationreason + aboutyou + activity + home;
+    // console.log(points);
+    // if(points < 99){
+    //   $("#pdx").show();
+    // } else if(points > 99 && points < 1000){
+    //   $("#ny").show();
+    // } else {
+    //   $("#hawaii").show();
+    // }
 
+    event.preventDefault();
+
+  });
 });
