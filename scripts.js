940eee5b31141198608322cfb3590e19c5e09fe8
5adiyah <sadiali@pdx.edu>
2016-02-24 20:47:35 -0800

    It works

--- scripts.js
+++ scripts.js
@@ -1,21 +1,28 @@
 $(document).ready(function(){
   $('#formwrapper form').submit(function(event){
-    var weather = parseInt($("#sweather").val());
-    var vacationreason = parseInt($("#svacationreason").val());
-    var aboutyou = parseInt($("#saboutyou").val());
-    var activity = parseInt($("#sactivity").val());
-    var home = parseInt($("#shome").val());
+    var weather = parseInt($("#sweather option:selected").val());
+    var vacationreason = parseInt($("#svacationreason option:selected").val());
+    var aboutyou = parseInt($("#saboutyou option:selected").val());
+    var activity = parseInt($("#sactivity option:selected").val());
+    var home = parseInt($("#shome option:selected").val());
 
-    // var points = weather + vacationreason + aboutyou + activity + home;
-    // console.log(points);
-    // if(points < 99){
-    //   $("#pdx").show();
-    // } else if(points > 99 && points < 1000){
-    //   $("#ny").show();
-    // } else {
-    //   $("#hawaii").show();
-    // }
+    var points = weather + vacationreason + aboutyou + activity + home;
 
+    if(points < 99){
+      $("#pdx").show();
+      $("#ny").hide();
+      $("#hawaii").hide();
+    } else if(points > 99 && points < 3000){
+      $("#pdx").hide();
+      $("#ny").show();
+      $("#hawaii").hide();
+    } else if(points > 3100){
+      $("#pdx").hide();
+      $("#ny").hide();
+      $("#hawaii").show();
+    }
+
+    console.log(points);
     event.preventDefault();
 
   });
